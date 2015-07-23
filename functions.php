dc6fd2e5b54a4f9f56c3176f468009bd51103715
Phil Cable <pcable@wsu.edu>
2015-07-23 08:24:17 -0700

    Enqueue dashicons stylesheet

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ add_action( 'wp_enqueue_scripts', 'people_scripts_styles', 11 );
  * Enqueue child theme Scripts and Styles
  */
 function people_scripts_styles() {
+	wp_enqueue_style( 'dashicons' );
 	if ( 'wsuwp_people_profile' === get_post_type() && is_single() ) {
 		wp_enqueue_script( 'wsuwp-people-profile-script', get_stylesheet_directory_uri() . '/js/profile.js', array( 'jquery' ), '', true );
 	}
