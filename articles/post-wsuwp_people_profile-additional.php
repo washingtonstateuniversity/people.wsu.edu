1eaae59e62f2730a9ebfc60e56d34a74e32f6d38
Phil Cable <pcable@wsu.edu>
2017-01-03 08:46:23 -0800

    Adjust throughout to meet PHP coding standards

--- articles/post-wsuwp_people_profile-additional.php
+++ articles/post-wsuwp_people_profile-additional.php
@@ -39,24 +39,24 @@ if ( $university_categories ||
 			</dt>
 			<dd>
 				<?php
-					if ( $university_categories && ! is_wp_error( $university_categories ) ) {
-						echo '<dl class="categorized">';
-						//echo '<dt><span class="categorized-default">Categorized</span></dt>';
-						foreach ( $university_categories as $category ) {
-							$category = sanitize_term( $category, 'wsuwp_university_category' );
-							echo '<dd><a href="' . esc_attr( get_term_link( $category, 'wsuwp_university_category' ) ) . '">' . esc_html( $category->name ) . '</a></dd>';
-						}
-						//echo '</dl>';
+				if ( $university_categories && ! is_wp_error( $university_categories ) ) {
+					echo '<dl class="categorized">';
+					//echo '<dt><span class="categorized-default">Categorized</span></dt>';
+					foreach ( $university_categories as $category ) {
+						$category = sanitize_term( $category, 'wsuwp_university_category' );
+						echo '<dd><a href="' . esc_attr( get_term_link( $category, 'wsuwp_university_category' ) ) . '">' . esc_html( $category->name ) . '</a></dd>';
 					}
-					
-					if ( has_tag() ) {
-						echo '<dl class="tagged">';
-						//echo '<dt><span class="tagged-default">Tagged</span></dt>';
-						foreach( get_the_tags() as $tag ) {
-							echo '<dd><a href="' . esc_attr( get_tag_link( $tag->term_id ) ) . '">' . esc_html( $tag->name ) . '</a></dd>';
-						}
-						echo '</dl>';
+					//echo '</dl>';
+				}
+
+				if ( has_tag() ) {
+					echo '<dl class="tagged">';
+					//echo '<dt><span class="tagged-default">Tagged</span></dt>';
+					foreach ( get_the_tags() as $tag ) {
+						echo '<dd><a href="' . esc_attr( get_tag_link( $tag->term_id ) ) . '">' . esc_html( $tag->name ) . '</a></dd>';
 					}
+					echo '</dl>';
+				}
 				?>
 			</dd>
 		</dl>
@@ -69,58 +69,57 @@ if ( $university_categories ||
 			</dt>
 			<dd>
 			<?php
-				if ( $cv_employment ) {
-					echo '<h3>Employment</h3>';
-					echo wpautop( wp_kses_post( $cv_employment ) );
-				}
+			if ( $cv_employment ) {
+				echo '<h3>Employment</h3>';
+				echo wp_kses_post( wpautop( $cv_employment ) );
+			}
 
-				if ( $cv_honors ) {
-					echo '<h3>Honors and Awards</h3>';
-					echo wpautop( wp_kses_post( $cv_honors ) );
-				}
+			if ( $cv_honors ) {
+				echo '<h3>Honors and Awards</h3>';
+				echo wp_kses_post( wpautop( $cv_honors ) );
+			}
 
-				if ( $cv_grants ) {
-					echo '<h3>Grants, Contracts, and Fund Generation</h3>';
-					echo wpautop( wp_kses_post( $cv_grants ) );
-					echo '<p class="key">Key to indicators or description of contributions to Grants, Contracts and Fund Generation: 1 = Provided the initial idea; 2 = Developed research/program design and hypotheses; 3 = Authored or co-authored grant application; 4 = Developed and/or managed budget; 5 = Managed personnel, partnerships, and project activities.</p>';
-				}
-				
-				if ( $cv_publications ) {
-					echo '<h3>Publications and Creative Work</h3>';
-					echo wpautop( wp_kses_post( $cv_publications ) );
-					echo '<p class="key">Key to indicators or description of contributions to Publications and Creative Work: 1 = Developed the initial idea; 2 = Obtained or provided funds or other resources; 3 = Collected data; 4 = Analyzed data; 5 = Wrote/created product; 6 = Edited product.</p>';
-				}
+			if ( $cv_grants ) {
+				echo '<h3>Grants, Contracts, and Fund Generation</h3>';
+				echo wp_kses_post( wpautop( $cv_grants ) );
+				echo '<p class="key">Key to indicators or description of contributions to Grants, Contracts and Fund Generation: 1 = Provided the initial idea; 2 = Developed research/program design and hypotheses; 3 = Authored or co-authored grant application; 4 = Developed and/or managed budget; 5 = Managed personnel, partnerships, and project activities.</p>';
+			}
 
-				if ( $cv_presentations ) {
-					echo '<h3>Presentations</h3>';
-					echo wpautop( wp_kses_post( $cv_presentations ) );
-				}
+			if ( $cv_publications ) {
+				echo '<h3>Publications and Creative Work</h3>';
+				echo wp_kses_post( wpautop( $cv_publications ) );
+				echo '<p class="key">Key to indicators or description of contributions to Publications and Creative Work: 1 = Developed the initial idea; 2 = Obtained or provided funds or other resources; 3 = Collected data; 4 = Analyzed data; 5 = Wrote/created product; 6 = Edited product.</p>';
+			}
 
-				if ( $cv_teaching ) {
-					echo '<h3>University Instruction</h3>';
-					echo wpautop( wp_kses_post( $cv_teaching ) );
-				}
+			if ( $cv_presentations ) {
+				echo '<h3>Presentations</h3>';
+				echo wp_kses_post( wpautop( $cv_presentations ) );
+			}
 
-				if ( $cv_service ) {
-					echo '<h3>Professional Service</h3>';
-					echo wpautop( wp_kses_post( $cv_service ) );
-				}
+			if ( $cv_teaching ) {
+				echo '<h3>University Instruction</h3>';
+				echo wp_kses_post( wpautop( $cv_teaching ) );
+			}
 
-				if ( $cv_responsibilities ) {
-					echo '<h3>Administrative Responsibility</h3>';
-					echo wpautop( wp_kses_post( $cv_responsibilities ) );
-				}
+			if ( $cv_service ) {
+				echo '<h3>Professional Service</h3>';
+				echo wp_kses_post( wpautop( $cv_service ) );
+			}
 
-				if ( $cv_societies ) {
-					echo '<h3>Professional and Scholarly Organization Affiliations</h3>';
-					echo wpautop( wp_kses_post( $cv_societies ) );
-				}
+			if ( $cv_responsibilities ) {
+				echo '<h3>Administrative Responsibility</h3>';
+				echo wp_kses_post( wpautop( $cv_responsibilities ) );
+			}
 
-				if ( $cv_professional_dev ) {
-					echo '<h3>Professional Developlment</h3>';
-					echo wpautop( wp_kses_post( $cv_professional_dev ) );
-				}
-			
+			if ( $cv_societies ) {
+				echo '<h3>Professional and Scholarly Organization Affiliations</h3>';
+				echo wp_kses_post( wpautop( $cv_societies ) );
+			}
+
+			if ( $cv_professional_dev ) {
+				echo '<h3>Professional Developlment</h3>';
+				echo wp_kses_post( wpautop( $cv_professional_dev ) );
+			}
 			?>
 			</dd>
 		</dl>
