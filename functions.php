974e7ab97bc9d4b5923e8dedd1132005a9e6004e
Phil Cable <pcable@wsu.edu>
2017-03-23 14:29:37 -0700

    Add handling for the people content type rewrites and permalinks
    I set the `rewrite` arg to `false` in the directory plugin - this
    will get us back to having people posts located at `/profile/slug`

--- functions.php
+++ functions.php
@@ -38,6 +38,8 @@ class WSUWP_People_Directory_Theme {
 	public function setup_hooks() {
 		add_filter( 'spine_child_theme_version', array( $this, 'theme_version' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 11 );
+		add_action( 'init', array( $this, 'rewrite_rules' ), 11 );
+		add_filter( 'post_type_link', array( $this, 'person_permalink'), 10, 2 );
 	}
 
 	/**
@@ -61,6 +63,32 @@ class WSUWP_People_Directory_Theme {
 		if ( 'wsuwp_people_profile' === get_post_type() && is_single() ) {
 			wp_enqueue_script( 'wsuwp-people-profile-script', get_stylesheet_directory_uri() . '/js/profile.js', array( 'jquery' ), $this->script_version, true );
 		}
+	}
+
+	/**
+	 * Add rewrite rules for handling people and person views.
+	 *
+	 * @since 0.1.0
+	 */
+	public function rewrite_rules() {
+		add_rewrite_tag( "%wsuwp_person%", '([^/]+)', WSUWP_People_Post_Type::$post_type_slug . '=' );
+		add_permastruct( 'person', "/profile/%wsuwp_person%/", false );
+	}
+
+	/**
+	 * Change the permalink structure for individual people posts.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $url  The post URL.
+	 * @param object $post The post object.
+	 */
+	public function person_permalink( $url, $post ) {
+		if ( 'wsuwp_people_profile' === get_post_type( $post ) ) {
+			$url = get_site_url() . '/profile/' . $post->post_name . '/';
+		}
+
+		return $url;
 	}
 }
 
