56a44835a036b97a8480e6c7b1373d0e159d241b
Phil Cable <pcable@wsu.edu>
2017-03-29 15:52:41 -0700

    Move the theme base class into its own file

---
+++ includes/class-wsuwp-people-directory-theme.php
@@ -0,0 +1,94 @@
+<?php
+
+class WSUWP_People_Directory_Theme {
+	/**
+	 * @since 0.1.0
+	 *
+	 * @var WSUWP_People_Directory_Theme
+	 */
+	private static $instance;
+
+	/**
+	 * @since 0.1.0
+	 *
+	 * @var string String used for busting cache on scripts.
+	 */
+	public $script_version = '0.1.0';
+
+	/**
+	 * Maintain and return the one instance and initiate hooks when
+	 * called the first time.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return \WSUWP_People_Directory_Theme
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new WSUWP_People_Directory_Theme();
+			self::$instance->setup_hooks();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Setup hooks to include.
+	 *
+	 * @since 0.1.0
+	 */
+	public function setup_hooks() {
+		add_filter( 'spine_child_theme_version', array( $this, 'theme_version' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 11 );
+		add_action( 'init', array( $this, 'rewrite_rules' ), 11 );
+		add_filter( 'post_type_link', array( $this, 'person_permalink' ), 10, 2 );
+	}
+
+	/**
+	 * Provide a theme version for use in cache busting.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return string
+	 */
+	public function theme_version() {
+		return $this->script_version;
+	}
+
+	/**
+	 * Enqueue child theme Scripts and Styles
+	 *
+	 * @since 0.1.0
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_style( 'dashicons' );
+		if ( 'wsuwp_people_profile' === get_post_type() && is_single() ) {
+			wp_enqueue_script( 'wsuwp-people-profile-script', get_stylesheet_directory_uri() . '/js/profile.js', array( 'jquery' ), $this->script_version, true );
+		}
+	}
+
+	/**
+	 * Add rewrite rules for handling people and person views.
+	 *
+	 * @since 0.1.0
+	 */
+	public function rewrite_rules() {
+		add_rewrite_tag( '%wsuwp_person%', '([^/]+)', WSUWP_People_Post_Type::$post_type_slug . '=' );
+		add_permastruct( 'person', '/profile/%wsuwp_person%/', false );
+	}
+
+	/**
+	 * Change the permalink structure for individual people posts.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $url  The post URL.
+	 * @param object $post The post object.
+	 */
+	public function person_permalink( $url, $post ) {
+		if ( 'wsuwp_people_profile' === get_post_type( $post ) ) {
+			$url = get_site_url() . '/profile/' . $post->post_name . '/';
+		}
+
+		return $url;
+	}
+}
