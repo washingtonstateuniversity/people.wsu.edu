613a857cb4263a27d7f31fe4b0c42b5f8593ca42
Phil Cable <pcable@wsu.edu>
2017-06-16 09:28:00 -0700

    Refactor person card markup and design

--- articles/post-wsuwp_people_profile.php
+++ articles/post-wsuwp_people_profile.php
@@ -1,151 +1,100 @@
-<?php if ( ! is_singular() ) : ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php endif; ?>
+<?php
+// AD data.
+$ad_title = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_title', true );
+$ad_email = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_email', true );
+$ad_phone = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_phone', true );
+$ad_phone_ext = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_phone_ext', true );
+$ad_office = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_office', true );
+$ad_address = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_address', true );
 
-	<header class="wsuwp-person-profile-header">
-		<hgroup>
-		<?php if ( is_single() ) : ?>
-			<?php if ( 'true' === spine_get_option( 'articletitle_show' ) ) : ?>
-				<h1 class="wsuwp-person-name"><?php the_title(); ?></h1>
-			<?php endif; ?>
-		<?php else : ?>
-			<h2 class="wsuwp-person-name">
-				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+// Override data.
+$alt_title = get_post_meta( get_the_ID(), '_wsuwp_profile_title', true );
+$alt_email = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_email', true );
+$alt_phone = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_phone', true );
+$alt_office = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_office', true );
+$alt_address = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_address', true );
+
+// Additional data.
+$degrees = get_post_meta( get_the_ID(), '_wsuwp_profile_degree', true );
+$photos = get_post_meta( get_the_ID(), '_wsuwp_profile_photos', true );
+$website = get_post_meta( get_the_ID(), '_wsuwp_profile_website', true );
+
+// Show the override data if it exists, otherwise show the AD data.
+$titles = ( $alt_title ) ? $alt_title : array( $ad_title );
+$email = ( $alt_email ) ? $alt_email : $ad_email;
+$office = ( $alt_office ) ? $alt_office : $ad_office;
+$phone = ( $alt_phone ) ? $alt_phone : $ad_phone;
+$address = ( $alt_address ) ? $alt_address : $ad_address;
+
+// Set up additional post classes.
+$classes = array( 'wsu-person' );
+
+// Define the URL of the primary photo.
+$photo_url = false;
+if ( $photos && is_array( $photos ) ) {
+	$photo_url = wp_get_attachment_image_src( $photos[0] )[0];
+} elseif ( has_post_thumbnail() ) {
+	$photo_url = get_the_post_thumbnail_url();
+}
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
+
+	<div class="card">
+
+		<header>
+
+			<?php if ( is_single() ) { ?>
+			<h1 class="name"><?php the_title(); ?></h1>
+			<?php } else { ?>
+			<h2 class="name">
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			</h2>
-		<?php endif; // is_single() or in_a_relationship() ?>
-		</hgroup>
-	</header>
-
-	<div class="wsuwp-person-card">
-
-		<?php
-		// Not sure where to put this yet.
-		/*if ( spine_has_featured_image() && ! is_singular() ) {
-			?><figure class="wsuwp-person-photo"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'spine-thumbnail_size' ); ?></a></figure><?php
-		}*/
-
-		// Meta data (excluding additional bios and C.V.).
-		$degrees    = get_post_meta( get_the_ID(), '_wsuwp_profile_degree', true );
-		$title      = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_title', true );
-		$titles     = get_post_meta( get_the_ID(), '_wsuwp_profile_title', true );
-		$phone      = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_phone', true );
-		$phone_ext  = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_phone_ext', true );
-		$alt_phone  = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_phone', true );
-		$office     = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_office', true );
-		$alt_office = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_office', true );
-		$address    = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_address', true );
-		$email      = get_post_meta( get_the_ID(), '_wsuwp_profile_ad_email', true );
-		$alt_email  = get_post_meta( get_the_ID(), '_wsuwp_profile_alt_email', true );
-		$cv         = get_post_meta( get_the_ID(), '_wsuwp_profile_cv', true );
-		$website    = get_post_meta( get_the_ID(), '_wsuwp_profile_website', true );
-
-		// Taxonomy data.
-		$departments     = wp_get_post_terms( get_the_ID(), 'wsuwp_university_org' );
-		$appointments    = wp_get_post_terms( get_the_ID(), 'appointment', array(
-			'fields' => 'names',
-		) );
-		$classifications = wp_get_post_terms( get_the_ID(), 'classification', array(
-			'fields' => 'names',
-		) );
-		$locations       = wp_get_post_terms( get_the_ID(), 'wsuwp_university_location', array(
-			'fields' => 'names',
-		) );
-
-		if ( is_single() && $degrees && is_array( $degrees ) ) {
-			?><ul class="wsuwp-person-education">
-			<?php foreach ( $degrees as $degree ) { ?>
-				<li class="degree"><?php echo esc_html( $degree ); ?></li>
 			<?php } ?>
-			</ul><?php
-		}
 
-		if ( is_single() && $classifications && ! is_wp_error( $classifications ) ) {
-			?><p class="wsuwp-person-classifications">
-			<?php
-			foreach ( $classifications as $classification ) {
-				echo esc_html( $classification );
-			}
-			?>
-			</p><?php
-		}
+			<?php if ( $degrees && is_array( $degrees ) ) { ?>
+				<?php foreach ( $degrees as $degree ) { ?>
+				<span class="degree"><?php echo esc_html( $degree ); ?></span>
+				<?php } ?>
+			<?php } ?>
 
-		if ( $title || $titles ) {
-		?><p class="wsuwp-person-position">
-		<?php
-		// Show user-entered title(s) if viewing full profile.
-		if ( is_single() && $titles ) {
-			foreach ( $titles as $additional_title ) :
-				?><span class="title"><?php echo esc_html( $additional_title ); ?></span><?php
-			endforeach;
-		} else {
-			if ( $title ) {
-				echo esc_html( $title );
-			}
-		}
-		?>
-		</p><?php
-		}
+		</header>
 
-		if ( $departments && ! is_wp_error( $departments ) && ( is_single() || is_search() || ( is_tax() && ! is_tax( 'wsuwp_university_org' ) ) ) ) {
-			?><p class="wsuwp-person-department">
-			<?php
-			foreach ( $departments as $department ) {
-				$dept = sanitize_term( $department, 'wsuwp_university_org' );
-				?><span class="department"><a href="<?php echo esc_attr( get_term_link( $dept, 'wsuwp_university_org' ) ); ?>"><?php echo esc_html( $dept->name ); ?></a></span><?php
-			}
-			?>
-			</p><?php
-		}
+		<?php if ( $photo_url ) { ?>
+		<figure class="photo">
+			<img src="<?php echo esc_url( $photo_url ); ?>" alt="<?php the_title(); ?>" />
+		</figure>
+		<?php } ?>
 
-		if ( $email || $alt_email ) {
-			?><p class="contact wsuwp-person-email"><span class="dashicons dashicons-email"></span>
-			<?php
-			if ( $email ) { echo '<a href="mailto:' . esc_attr( $email ) . '">' . esc_html( $email ) . '</a>'; }
-			if ( is_single() && $email && $alt_email ) { echo ' | '; }
-			if ( is_single() && $alt_email ) { echo '<a href="mailto:' . esc_attr( $alt_email ) . '">' . esc_html( $alt_email ) . '</a>'; }
-			?>
-			</p><?php
-		}
+		<div class="contact">
 
-		if ( $phone || $alt_phone ) {
-			?><p class="contact wsuwp-person-phone"><span class="dashicons dashicons-phone"></span>
-			<?php
-			if ( $phone ) { echo esc_html( $phone ); }
-			if ( is_single() && $phone && $alt_phone ) { echo ' | '; }
-			if ( is_single() && $alt_phone ) { echo esc_html( $alt_phone ); }
-			?>
-			</p><?php
-		}
+			<?php if ( $titles && is_array( $titles ) ) { ?>
+				<?php foreach ( $titles as $title ) { ?>
+				<span class="title"><?php echo esc_html( $title ); ?></span>
+				<?php } ?>
+			<?php } ?>
 
-		if ( $office || $alt_office || $locations ) {
-			?><p class="contact wsuwp-person-office"><span class="dashicons dashicons-location"></span>
-			<?php
-			if ( $office ) { echo esc_html( $office ); }
-			if ( is_single() && $office && $alt_office ) { echo ' | '; }
-			if ( is_single() && $alt_office ) { echo esc_html( $alt_office ); }
-			if ( $address ) { echo '<span class="address">' . esc_html( $address ) . '</span>'; }
-			if ( $locations && ! is_wp_error( $locations ) ) {
-				foreach ( $locations as $location ) {
-					echo '<span class="address">' . esc_html( $location ) . '</span>';
-				}
-			}
-			?>
-			</p><?php
-		}
+			<span class="email"><a href="mailto:<?php echo esc_attr( $email ); ?>"><?php echo esc_html( $email ); ?></a></span>
 
-		// Curriculum Vitae.
-		if ( is_single() && $cv ) {
-			?><p class="contact cv"><span class="dashicons dashicons-download"></span><a href="<?php echo esc_url( wp_get_attachment_url( $cv ) ); ?>">Curriculum Vitae</a></p><?php
-		}
+			<span class="phone"><?php echo esc_html( $phone ); ?></span>
 
-		// Website.
-		if ( is_single() && $website ) {
-			?><p class="contact website"><span class="dashicons dashicons-external"></span><a href="<?php echo esc_url( $website ); ?>">Website</a></p><?php
-		}
-		?>
+			<span class="office"><?php echo esc_html( $office ); ?></span>
 
-	</div><!-- .wsuwp-person-card -->
+			<?php if ( $address ) { ?>
+			<span class="address"><?php echo esc_html( $address ); ?></span>
+			<?php } ?>
 
-<?php if ( ! is_singular() ) : ?>
+			<?php if ( $website ) { ?>
+			<span class="website"><a href="<?php echo esc_url( $website ); ?>"><?php echo esc_html( $website ); ?></a></span>
+			<?php } ?>
+		</div>
+
+	</div>
+
+	<?php if ( is_single() ) { ?>
+	<div class="about">
+		<?php the_content(); ?>
+	</div>
+	<?php } ?>
+
 </article>
-<?php endif; ?>
--- js/profile.js
+++
@@ -1,9 +0,0 @@
-(function($){
-
-	$('.wsuwp-person-additional > dl > dd').hide();
-
-	$('.wsuwp-person-additional > dl > dt').click( function() {
-		$(this).next('dd').toggle().parents('dl').toggleClass('disclosed');
-	})
-
-}(jQuery));
